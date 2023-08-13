7b7d592a8988e25aa6a4c3a7576fe44af58024cd
moawezz <moawez.de@gmail.com>
2023-08-13 13:52:15 +0330

    Add post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import posts_list_view
+from .views import posts_list_view, post_detail_view
 
 urlpatterns = [
-    path('', posts_list_view, name='posts_list')
+    path('', posts_list_view, name='posts_list'),
+    
+    # primary key
+    path('<int:pk>/', post_detail_view, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Post
 
 def posts_list_view(request):
     posts = Post.objects.all()
     context = {'posts': posts}
 
-    return render(request, 'blog/post_list.html', context)
+    return render(request, 'blog/posts_list.html', context)
+
+def post_detail_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post': post}
+
+    return render(request, 'blog/post_detail.html',  context)
