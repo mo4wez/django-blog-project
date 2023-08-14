cb367144d486168afa8fc7b8cf47c38deea90a59
moawezz <moawez.de@gmail.com>
2023-08-14 15:48:40 +0330

    Add Create Post view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import posts_list_view, post_detail_view
+from .views import posts_list_view, post_detail_view, create_post_view
 
 urlpatterns = [
     path('', posts_list_view, name='posts_list'),
-
     # primary key
     path('<int:pk>/', post_detail_view, name='post_detail'),
+    path('create/', create_post_view, name='create_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 from .models import Post
 
 def posts_list_view(request):
@@ -13,3 +15,14 @@ def post_detail_view(request, pk):
     context = {'post': post}
 
     return render(request, 'blog/post_detail.html',  context)
+
+def create_post_view(request):
+    if request.method == 'POST':
+        post_title = request.POST.get('title')
+        post_text = request.POST.get('text')
+
+        user = User.objects.all()[0]
+        Post.objects.create(title=post_title, text=post_text, author=user, status='pub')
+
+
+    return render(request, 'blog/create_post.html')
